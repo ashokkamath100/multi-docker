dd222080fe943c575f842607687fcc3d7438f67c
Ashok Kamath <ashokkamath@Ashoks-MacBook-Pro.local>
2024-08-10 20:37:00 -0400

    made change

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-          <Link to="/" >Home</Link>
+          <Link to="/" >Fack</Link>
           <Link to="/otherpage">Other Page</Link>
         <div>
             <Route exact path="/" component={Fib}  />
